1154e89441ee7505f0cd12419af690429d458de5
Benjamin Toll <benjam72@yahoo.com>
2015-05-17 15:07:55 -0400

    Introduced jsLite

--- js/home.js
+++ js/home.js
@@ -1,33 +1,19 @@
-(function () {
-    var links, title,
+JSLITE.ready(function () {
+    var links = JSLITE.dom.gets('a'),
+        title;
 
-    removeTitle = function () {
+    function removeTitle() {
         // Neuter the default browser behavior.
         title = this.title;
         this.title = '';
-    },
+    }
 
-    resetTitle = function () {
+    function resetTitle() {
         // Restore the title.
         this.title = title;
-    },
+    }
 
-    onDOMContentLoaded = function () {
-        var len, i;
-
-        links = document.querySelectorAll('a');
-
-        for (i = 0, len = links.length; i < len; i++) {
-            (function (i) {
-                var link = links[i];
-
-                link.addEventListener('mouseover', removeTitle);
-                link.addEventListener('mouseout', resetTitle);
-            }(i));
-        }
-
-    };
-
-    document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
-}());
+    links.on('mouseover', removeTitle);
+    links.on('mouseout', resetTitle);
+});
 
