180ffd8e088c3bb3933cacefa20fbf1064511b66
Benjamin Toll <benjam72@yahoo.com>
2015-05-18 14:48:00 -0400

    Changed nav.css to use flex layout. Replaced jsLite scripts with minimized build script in head.php.

--- server/head.php
+++ server/head.php
@@ -2,19 +2,7 @@
 $head = <<<HEAD
 <title>The Owl's Nest Farm</title>
 <link rel="stylesheet" type="text/css" href="/owlsnestfarm/resources/css/main.css" />
-<script type="text/javascript" src="/jsLite/lib/js/JSLITE.prototype.js"></script>
-<script type="text/javascript" src="/jsLite/lib/js/JSLITE.js"></script>
-<script type="text/javascript" src="/jsLite/lib/js/JSLITE.Element.js"></script>
-<script type="text/javascript" src="/jsLite/lib/js/JSLITE.Composite.js"></script>
-<script type="text/javascript" src="/jsLite/lib/js/JSLITE.Observer.js"></script>
-<script type="text/javascript" src="/jsLite/lib/js/JSLITE.Rules.js"></script>
-<script type="text/javascript" src="/jsLite/lib/js/JSLITE.Template.js"></script>
-<script type="text/javascript" src="/jsLite/lib/js/JSLITE.util.js"></script>
-<script type="text/javascript" src="/jsLite/lib/js/JSLITE.dom.js"></script>
-<script type="text/javascript" src="/jsLite/lib/js/JSLITE.domQuery.js"></script>
-<script type="text/javascript" src="/jsLite/lib/js/JSLITE.ajax.js"></script>
-<script type="text/javascript" src="/jsLite/lib/js/JSLITE.log.js"></script>
-<script type="text/javascript" src="/jsLite/lib/js/JSLITE.ux.js"></script>
+<script src="/owlsnestfarm/js/jslite.js"></script>
 HEAD;
 
 echo $head;
