339b5bdce4dbeb54334eceed21bafec891d46b52
Benjamin Toll <benjam72@yahoo.com>
2015-05-19 12:10:59 -0400

    - Added initial About Us text - Renamed js/home.js to js/main.js - Added js/main.js to HEAD of main pages

--- server/head.php
+++ server/head.php
@@ -3,6 +3,7 @@ echo <<<HEAD
 <title>The Owl's Nest Farm</title>
 <link rel="stylesheet" type="text/css" href="/owlsnestfarm/resources/css/main.css" />
 <script src="/owlsnestfarm/js/jslite.js"></script>
+<script src="/owlsnestfarm/js/main.js"></script>
 HEAD;
 ?>
 
--- server/nav.php
+++ server/nav.php
@@ -2,12 +2,12 @@
 echo <<<NAV
     <nav>
         <ul>
-            <li><a href="/owlsnestfarm/">Home</a></li>
-            <li><a href="/owlsnestfarm/about/">About Us</a></li>
-            <li><a href="/owlsnestfarm/history/">History</a></li>
-            <li><a href="/owlsnestfarm/animals/">The Animals</a></li>
-            <li><a href="/owlsnestfarm/market/">Farmer's Market</a></li>
-            <li><a href="/owlsnestfarm/contact/">Contact Us</a></li>
+            <li><a href="/owlsnestfarm/" title="Home">Home</a></li>
+            <li><a href="/owlsnestfarm/about/" title="About Us">About Us</a></li>
+            <li><a href="/owlsnestfarm/history/" title="History">History</a></li>
+            <li><a href="/owlsnestfarm/animals/" title="Animals">The Animals</a></li>
+            <li><a href="/owlsnestfarm/market/" title="Farmer's Market">Farmer's Market</a></li>
+            <li><a href="/owlsnestfarm/contact/" title="Contact Us">Contact Us</a></li>
         </ul>
     </nav>
 NAV;
