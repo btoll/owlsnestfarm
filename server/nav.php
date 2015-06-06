f26f64a89a6a552bdb41d318f4ec3ea4631f2358
Benjamin Toll <benjam72@yahoo.com>
2015-06-06 18:22:04 -0400

    Added new nav bar to the main pages

--- server/nav.php
+++ server/nav.php
@@ -1,16 +1,16 @@
 <?php
 echo <<<NAV
     <nav>
-        <ul>
-            <li><a href="/owlsnestfarm/" title="Home">Home</a></li>
-            <li><a href="/owlsnestfarm/about/" title="About Us">About Us</a></li>
-            <!--
-            <li><a href="/owlsnestfarm/history/" title="History">History</a></li>
-            <li><a href="/owlsnestfarm/animals/" title="Animals">The Animals</a></li>
-            <li><a href="/owlsnestfarm/market/" title="Farmer's Market">Farmer's Market</a></li>
-            -->
-            <li><a href="/owlsnestfarm/contact/" title="Contact Us">Contact Us</a></li>
-        </ul>
+        <div class="nav-container">
+            <ul>
+                <li><a href="/owlsnestfarm/about/" title="About Us">About Us</a></li>
+                <li><a href="/owlsnestfarm/history/" title="History">History</a></li>
+                <li><a href="/owlsnestfarm/animals/" title="Animals">The Animals</a></li>
+                <li><a href="/owlsnestfarm/market/" title="Farmer's Market">Farmer's Market</a></li>
+                <li><a href="/owlsnestfarm/contact/" title="Contact Us">Contact Us</a></li>
+                <li><a href="/owlsnestfarm/" title="Home">Home</a></li>
+            </ul>
+        </div>
     </nav>
 NAV;
 ?>
