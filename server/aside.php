7eee729a0cf14867342e75ba22427851a10d6f39
Benjamin Toll <benjam72@yahoo.com>
2015-06-10 02:01:36 -0400

    Added Facebook icon and fiddled with styles for the address element that contains it

--- server/aside.php
+++ server/aside.php
@@ -1,17 +1,25 @@
 <?php
 echo <<<ASIDE
 <aside>
+            <!--
             <address>
                 <p>The Owl's Nest Farm</p>
                 <p>Princeton, Massachusetts 01541</p>
             </address>
+            -->
 
             <figure>
                 <img src="../resources/images/owl_house_plaque_small.png">
             </figure>
 
             <address>
-                <a href="https://www.facebook.com/Theowlsnestfarm" title="The Owl's Nest Farm on Facebook" target="_blank">Visit us on Facebook</a> for hours and directions to our farmstand.
+                <div>
+                    <a href="https://www.facebook.com/Theowlsnestfarm" title="The Owl's Nest Farm on Facebook" target="_blank"><img src="../resources/images/facebook_smallest.png"></a>
+                </div>
+
+                <p>
+                    <a href="https://www.facebook.com/Theowlsnestfarm" title="The Owl's Nest Farm on Facebook" target="_blank">Visit us on Facebook</a> for hours and directions to our farmstand.
+                </p>
             </address>
         </aside>
 ASIDE;
