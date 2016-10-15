492ce44fbb6023b0368aaa47e243275c6eb4de85
Benjamin Toll <benjam72@yahoo.com>
2016-10-15 18:47:32 -0400

    Remove Hunt and Gather

--- about/index.php
+++ about/index.php
@@ -25,7 +25,6 @@ include "../server/head.php";
                 <ul>
                     <li><a href="http://www.elziresacre.com/" title="Elzire&rsquo;s Acre" target="_blank">Elzire&rsquo;s Acre</a></li>
                     <li><a href="https://www.facebook.com/EncoresCloset/" title="Encore&rsquo;s Closet" target="_blank">Encore&rsquo;s Closet</a></li>
-                    <li><a href="http://huntandgathervintagemarket.com/" title="Hunt &amp; Gather Vintage Market" target="_blank">Hunt &amp; Gather Vintage Market</a></li>
                     <li><a href="http://www.princetonpubliclibrary.org/" title="Princeton Public Library" target="_blank">Princeton Public Library</a></li>
                     <li><a href="https://www.facebook.com/Shamrock-Hives-Farm-211386495588210/" title="Shamrock Hives Farm" target="_blank">Shamrock Hives Farm</a></li>
                     <li><a href="https://www.facebook.com/simplesweetsbysusan/" title="Simple Sweets by Susan" target="_blank">Simple Sweets by Susan</a></li>
--- index.php
+++ index.php
@@ -11,7 +11,6 @@ include "./server/head.php";
         <?php include "./server/aside.php"; ?>
 
         <article>
-            <p>We have a stand at the <a href="http://huntandgathervintagemarket.com/" title="Hunt and Gather Vintage Market" target="_blank">Hunt and Gather Vintage Market</a> in Princeton, MA. Please stop by and say "hi" starting Sunday, May 22th! Hours are from 1pm to 4pm.</p>
             <p>In addition to our fresh produce, we can bake custom-order dog treats for any special needs. Don't hesitate to ask!</p>
             <p class="bold">Currently in season:
                 <ul>
