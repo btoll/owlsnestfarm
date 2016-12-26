bf62c90c4ed1e2e8545f6fb0b75e344728783864
Benjamin Toll <benjam72@yahoo.com>
2016-12-26 14:52:28 -0500

    Added info about winter market

--- index.php
+++ index.php
@@ -11,6 +11,11 @@ include "./server/head.php";
         <?php include "./server/aside.php"; ?>
 
         <article>
+            <p>January 15th starts our indoor Winter market!</p>
+            <P>Join us at the Congregational Church of Princeton on the 1st and 3rd Sundays of each month through May from 11:30am to 2pm.</p>
+            <p>More than 10 local vendors will be available each market to satisfy your family's every need!</p>
+            <p>For more information and current news, please visit <a href="https://www.facebook.com/Princetonfarmersmarketgroup">Princeton Farmers' Market Group</a> on Facebook.</p>
+
             <p>In addition to our fresh produce, we can bake custom-order dog treats for any special needs. Don't hesitate to ask!</p>
             <!--
             <p class="bold">Currently in season:
