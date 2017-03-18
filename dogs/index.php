55f0b121ca5d60ece931821a8ce763068f817907
Benjamin Toll <benjam72@yahoo.com>
2017-03-17 21:29:25 -0400

    Added content to dogs/

--- dogs/index.php
+++ dogs/index.php
@@ -11,7 +11,40 @@ include "../server/head.php";
         <?php include "../server/aside.php"; ?>
 
         <article>
-        <p>Under construction</p>
+            <p>Choose from three different yummy and healthy treats!</p>
+            <p>Each variety is 100% organic and vegan! (shh, don't tell the puppies)</p>
+            <div>
+                <p>Pete's Senior Softies (grain-free)
+                    <ul>
+                        <li>Carrots (high in Vitamins A, K, C &amp; B6)</li>
+                        <li>Dates (great sources of potassium, protein &amp; magnesium)</li>
+                        <li>Peas (high in Vitamins K, B1 &amp; C, manganese &amp; fiber)</li>
+                        <li>PB Fit (low-calorie, great taste &amp; protein)</li>
+                        <li>Quinoa Flour (grain-free)</li>
+                    </ul>
+                </p>
+            </div>
+            <div>
+                <p>Sweet Molly Chews
+                    <ul>
+                        <li>Sweet Potatoes (antioxidants &amp; Vitamins A, C &amp; B6)</li>
+                        <li>Coconut Oil (great for skin &amp; coats, helps to reduce bad breath)</li>
+                        <li>Cinnamon (for sensitive stomachs)</li>
+                    </ul>
+                </p>
+            </div>
+            <div>
+                <p>Utley's Oaties
+                    <ul>
+                        <li>Sweet Potatoes (antioxidants &amp; Vitamins A, C &amp; B6)</li>
+                        <li>Old-fashioned rolled oats (high fiber / low fat)</li>
+                        <li>Organic PB Fit</li>
+                        <li>Organic whole wheat flour</li>
+                        <li>Bananas (loads of vitamins)</li>
+                        <li>Cinnamon (for sensitive stomachs</li>
+                    </ul>
+                </p>
+            </div>
         </article>
 
         <!--
